ecb624b54975409336f10d7688bfa08971da0dbe
sergezak <sergezak9@gmail.com>
2024-03-30 13:40:38 +0300

    created CasgFlow table

---
+++ database/migrations/2024_03_30_103521_create_cash_flows_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (Schema::hasTable('cash_flows')) {
+            return;
+        }
+
+        Schema::create('cash_flows', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('name', 255)->nullable();
+            $table->float('amount');
+            $table->text('note')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cash_flows');
+    }
+};
