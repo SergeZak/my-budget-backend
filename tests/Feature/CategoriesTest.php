e5661290182486c027c9c59dc89b269a83c8b220
sergezak <sergezak9@gmail.com>
2024-03-30 13:31:32 +0300

    added Auth routes

--- tests/Feature/CategoriesTest.php
+++ tests/Feature/CategoriesTest.php
@@ -16,7 +16,7 @@ class CategoriesTest extends TestCase
      * @test
      * @return void
      */
-    public function user_can_list_categories()
+    public function user_can_list_categories(): void
     {
         $this->loginUser();
         $categories = $this->categoryFactory->count(3)
@@ -44,7 +44,7 @@ class CategoriesTest extends TestCase
      * @test
      * @return void
      */
-    public function user_can_create_a_category()
+    public function user_can_create_a_category(): void
     {
         $this->loginUser();
 
@@ -58,7 +58,7 @@ class CategoriesTest extends TestCase
      * @test
      * @return void
      */
-    public function user_can_update_a_category()
+    public function user_can_update_a_category(): void
     {
         $this->withoutExceptionHandling();
         $this->loginUser();
@@ -79,7 +79,7 @@ class CategoriesTest extends TestCase
      * @test
      * @return void
      */
-    public function user_can_delete_a_category()
+    public function user_can_delete_a_category(): void
     {
         $this->loginUser();
 
@@ -87,5 +87,26 @@ class CategoriesTest extends TestCase
 
         $this->post(route('categories.delete', ['category' => $category->id]))->assertOk();
         $this->assertDatabaseMissing('categories', $category->toArray());
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function only_auth_user_can_perform_actions_on_categories(): void
+    {
+        $categories = $this->categoryFactory->count(3)->create();
+        $this->get(route('categories.index'))->assertRedirect(route('login'));
+
+
+        $params = [
+            'title' => $this->faker->sentence(2),
+        ];
+        $this->post(route('categories.update', ['category' => $categories[0]->id]), $params)
+            ->assertRedirect(route('login'));
+
+        $this->post(route('categories.delete', ['category' => $categories[0]->id]))
+            ->assertRedirect(route('login'));
+
     }
 }
